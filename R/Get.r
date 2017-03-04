3c06862eaf4ee28f9258f2d014ba920a8cc91245
muffin1974 <s.voigt1988@gmx.net>
2017-03-04 16:33:26 +0100

    Initialized lykke package

---
+++ R/Get.r
@@ -0,0 +1,10 @@
+#' @export
+#' @importFrom httr GET
+Get <- function() {
+
+base_url <- 'https://api.lykkex.com/api/' 
+get_url<-paste0(base_url,'Root',"?")
+res <-  GET (get_url)
+return(content(res)) 
+
+}
---
+++ R/download_lykke_specs.R
@@ -0,0 +1,31 @@
+#' download_lykke_specs
+#'
+#' @importFrom jsonlite fromJSON
+#' @export
+
+download_lykke_specs	<- function(){
+	g<-fromJSON('https://api.lykkex.com/swagger/v1/swagger.json')
+ 	message(sprintf("Extract data for package %s...", 'lykke'))
+	
+	for (ops in names(g$paths)) {
+    		for (op in names(g$paths[[ops]])) {
+	  	    node <- g$paths[[ops]][[op]]
+	 	    fil <- paste("./R", sprintf("%s.r", node$operationId), sep="/")
+ 		    message(sprintf("  - creating %s...", fil))
+ 		    cat("#' @export\n", file=fil, append=TRUE)
+ 		    cat("#' @importFrom base NULL\n", file=fil, append=TRUE)
+ 		    cat("#' @importFrom httr GET\n", file=fil, append=TRUE)
+		    if(is.NULL(node$parameters$name)){
+			    cat(sprintf("%s <- function(%s) {\n\n",
+      	            	node$operationId,
+					paste(node$parameters$name, collapse=", ")), file=fil, append=TRUE)
+					cat("base_url <- 'https://api.lykkex.com/api/' \n", file=fil,append=TRUE)
+	   		      	cat(paste0("get_url<-paste0(base_url,","'",node$tags,"'",",'?',param,'=',",node$parameters$name,")\n"), file=fil, append=TRUE)
+				}
+				get_url<-paste0(base_url,node$tags,'?',param,'=',node$parameters$name)
+			      cat("res <- ", toupper(op),'(get_url)\n', file=fil, append=TRUE)
+			      cat('return(content(res)) \n', file=fil, append=TRUE)
+				cat("\n}\n", file=fil, append=TRUE)
+
+	}}
+	}	
